f00ef94569708543baf7cfd5052a7efb3e678ef4
Smirnov Aleksandr <mail_er@mail.ru>
2019-11-24 14:40:44 +0700

    Добавил bootstrap в проект

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,20 @@
-<html>
-    <head>
-        <title>@yield('title') - {{ config('app.name') }}</title>
-    </head>
-    <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <title>@yield('title') - {{ config('app.name') }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/app.js"></script>
+  </head>
+  <body>
+    @section('sidebar')
+      This is the master sidebar.
+    @show
 
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
